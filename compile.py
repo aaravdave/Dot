d4e32bfcf8352b3bb148382432b1facb0f27f8b5
Aarav Dave <aarinaarav@gmail.com>
2021-02-11 20:21:04 -0500

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,15 +1,16 @@
-variables = {}
+def run(code):
+    variables = {}
 
-with open('code.dscr') as file:
-    for line in file:
-        line = line.strip().split()
-        if ''.join(line) == '':
-            continue
-        elif line[0].startswith('#'):
-            continue
-        elif 'equals' in line:
-            variables[line[0]] = line[2]
-        elif line[0] == 'say':
-            for i in line[1:]:
-                print(variables[i.strip('[]')] if i.startswith('[') and i.endswith(']') else i, end=' ')
-            print()
+    with code as file:
+        for line in file:
+            line = line.strip().split()
+            if ''.join(line) == '':
+                continue
+            elif line[0].startswith('#'):
+                continue
+            elif 'equals' in line:
+                variables[line[0]] = line[2]
+            elif line[0] == 'say':
+                for i in line[1:]:
+                    print(variables[i.strip('[]')] if i.startswith('[') and i.endswith(']') else i, end=' ')
+                print()
