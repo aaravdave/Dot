3f5b84eb0e4ea3a84f2998304a8f11550e890efe
Aarav Dave <aarinaarav@gmail.com>
2021-02-23 18:25:48 -0500

    Update compile.py

--- compile.py
+++ compile.py
@@ -12,7 +12,7 @@ if_in = ''
 
 
 def check_var(string):
-    return variables[string.strip('[]')] if string.startswith('[') and string.endswith(']') else string
+    return variables[string.strip('[]')] if string.startswith('[') and string.endswith(']') else int(string.strip('_')) if string.startswith('_') and string.endswith('_') else string
 
 
 def eval_condition(line):
@@ -45,10 +45,17 @@ def run(code):
             try:
                 line = line.strip().split()
                 if ''.join(line) == '' or len(line) != 0 and line[0].startswith('#'):
+                    line_num += 1
                     continue
                 elif 'if' == line[0]:
                     if_in = 'if'
                     current_condition = line[1:]
+                    line_num += 1
+                    continue
+                elif 'repeat' == line[0]:
+                    if_in = f'for {line[3]} {line[1]}'
+                    variables[line[3]] = 1
+                    line_num += 1
                     continue
                 elif 'end' == ''.join(line):
                     if if_in == '':
@@ -56,18 +63,23 @@ def run(code):
                         break
                     else:
                         if_in = ''
-                if if_in == '' or if_in == 'if' and eval_condition(current_condition):
+                        line_num += 1
+                        continue
+                while if_in == '' or if_in == 'if' and eval_condition(current_condition) or len(if_in.split()) == 3 and if_in.split()[0] == 'for' and variables[if_in.split()[1]] != int(if_in.split()[2]) + 1:
                     if 'equals' in line:
                         variables[line[0]] = line[2]
                     elif line[0] == 'say':
                         text = Label(window, text=[check_var(i) for i in line[1:]])
                         text.place(x=0, y=window_line * 20, height=20)
                         window_line += 1
-            except Exception as e:
+                    if if_in and if_in.split()[0] == 'for':
+                        variables[if_in.split()[1]] += 1
+                    else:
+                        line_num += 1
+                        break
+            except Exception:
                 return_error(line)
                 break
-            line_num += 1
-
     done = Label(window, text=f'Exit Code {error}')
     done.place(x=0, y=window_line * 20, height=20)
     window_line += 2
