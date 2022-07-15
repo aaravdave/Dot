74329d6b45007a3f282155e4a4cf77b12d0fd2c7
Aarav Dave <aarinaarav@gmail.com>
2022-07-15 12:02:06 -0400

    Update dscr.py
    Line numbers now included.

--- dscr.py
+++ dscr.py
@@ -14,7 +14,9 @@ window.geometry('800x600')
 window.title('DSCR')
 
 entry = Text(window)
-entry.place(x=0, y=25, width=800, height=575)
+entry.place(x=20, y=25, width=800, height=575)
+line_numbers = Text(window, state=DISABLED)
+line_numbers.place(x=0, y=25, width=20, height=575)
 
 run = Button(window, text='>', command=start)
 run.place(x=775, y=0, width=25, height=25)
@@ -27,4 +29,9 @@ variable.set(options[0])
 choose_file = OptionMenu(window, variable, *options)
 choose_file.place(x=675, y=0, width=100, height=25)
 
-window.mainloop()
+while True:
+    line_numbers.configure(state=NORMAL)
+    line_numbers.delete('1.0', END)
+    line_numbers.insert(END, '\n'.join(str(i + 1) for i in range(entry.get('1.0', END).count('\n'))))
+    line_numbers.configure(state=DISABLED)
+    window.update()
